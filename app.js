a170560c4e36a3b72c26f3f3d63412deb2e2dd51
LyndonSimpson <lyndon.simpson@gmail.com>
2023-03-28 03:17:48 +0200

    translation in progress

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
+import translations from './translations.js';
+
 document.addEventListener('DOMContentLoaded', function () {
+  const langToggle = document.getElementById('lang-toggle');
   const themeToggle = document.getElementById('theme-toggle');
   const body = document.body;
+  let currentLang = 'fr';
+
+  function updateTranslations() {
+    const translatedElements = document.querySelectorAll('[data-translate]');
+
+    translatedElements.forEach((element) => {
+      const translationKey = element.getAttribute('data-translate');
+      element.textContent = translations[currentLang][translationKey];
+    });
+  }
 
   function updateButtonLabel() {
     if (body.classList.contains('dark')) {
@@ -10,11 +23,18 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   }
 
+  langToggle.addEventListener('click', function () {
+    currentLang = currentLang === 'fr' ? 'en' : 'fr';
+    updateTranslations();
+  });
+
   themeToggle.addEventListener('click', function () {
     body.classList.toggle('dark');
     updateButtonLabel();
   });
 
-  // Set the initial button label
+  // Set the initial translations and button label
+  updateTranslations();
   updateButtonLabel();
 });
+
---
+++ translations.js
@@ -0,0 +1,52 @@
+export default {
+    en: {
+        header_title: "Lyndon Simpson",
+        header_subtitle: "Web and Web Mobile Developer",
+        education_title: "Education",
+        o_clock_title: "O’clock, Paris — Web Developer Certification Level 5 (Associate Degree)",
+        o_clock_date: "June 2022 to November 2022 - Professional title obtained in February 2023",
+        icp_title: "ICP, Paris — Bachelor of Modern Literature (Bachelor's degree)",
+        icp_date: "Degree obtained in 2017 - Poitiers Faculty",
+        skills_title: "Skills",
+        skills_lang: "Languages: HTML, CSS, JavaScript, SQL",
+        skills_techno: "Technologies: NodeJs, PostgreSQL, Express",
+        skills_languages: "Languages: Bilingual english",
+        skills_soft: "Soft skills: Autonomous, versatile, and passionate about learning",
+        experiences_title: "Experiences",
+
+        // insert missing translation here -lines 73 to 94 in html
+
+        projects_title: "Projects",
+        project1_title: "Mhumain, Remote work — Lead back-end dev, git master October - November 2022, Professional project",
+        project1_repo: "GitHub Repo",
+        project2_title: "Dynamic Diary API Project",
+        project2_description: "Personal project that imitates a diary but makes it dynamic and offers data manipulation that allows for more intuitive navigation through memories",
+        project2_status: "Project under construction"
+      },
+      fr: {
+        header_title: "Lyndon Simpson",
+        header_subtitle: "Développeur web et web mobile",
+        education_title: "Formation",
+        o_clock_title: "O’clock, Paris — Certification développeur web niveau 5 (Bac +2)",
+        o_clock_date: "Juin 2022 à Novembre 2022 - Titre professionnel obtenu en février 2023",
+        icp_title: "ICP, Paris — Licence de Lettres modernes",
+        icp_date: "Diplôme obtenu en 2017 - Faculté de Poitiers",
+        skills_title: "Compétences",
+        skills_lang: "Langages: HTML, CSS, JavaScript, SQL",
+        skills_techno: "Technologies: NodeJs, PostgreSQL, Express",
+        skills_languages: "Langues: Anglais bilingue",
+        skills_soft: "Soft skills: Autonome, polyvalent et passionné d'apprentissage",
+        experiences_title: "Expériences",
+
+        // insert missing translation here -lines 73 to 94 in html
+
+        projects_title: "Projets",
+        project1_title: "Mhumain, Télétravail — Lead dev back-end, git master Octobre - Novembre 2022, Projet professionnel",
+        project1_repo: "Dépôt GitHub",
+        project2_title: "Projet API journal intime dynamique",
+        project2_description: "Projet personnel qui imite un journal intime, mais le rend dynamique et propose une manipulation des données permettant une navigation plus intuitive à travers les souvenirs",
+        project2_status: "Projet en construction"
+    }
+    
+};
+
