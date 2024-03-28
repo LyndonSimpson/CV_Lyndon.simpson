3b7fe9cad4ac8e029ec486afd64515f37af9fc0f
LyndonSimpson <lyndon.simpson@gmail.com>
2024-03-28 14:28:56 +0100

    fix

---
+++ fake-terminal/js/commands.js
@@ -0,0 +1,85 @@
+var portfolio = "https://lyndonsimpson.github.io/CV_Lyndon.simpson/";
+var resume = "https://lyndonsimpson.github.io/CV_Lyndon.simpson/RESUME/index.html";
+var password = "admin";
+var linkedin = "https://www.linkedin.com/in/lyndon-simpson-00248226b/";
+var game = "https://lyndonsimpson.github.io/CV_Lyndon.simpson/La-Clique-Simulator/welcomepage.html";
+var github = "https://github.com/LyndonSimpson/";
+var email = 'mailto:lyndon.simpson@gmail.com';
+
+whois = [
+    "<br>",
+    "Welcome to my portfolio! I'm a junior full-stack web and mobile developer,",
+    "with a particular focus on back-end development. I earned a professional title",
+    "(level 5, equivalent to a 2-year degree) in February 2023 after an intensive",
+    "training with O'clock school. My end-of-training project, for which I developed",
+    "the entire back-end, was a website with secure authentication, database, and",
+    "an internal messaging system.",
+    "I've developed a keen interest in video game development, exploring various",
+    "frameworks and tools. My experience with Three.js and Blender has deepened my",
+    "understanding of 3D modeling and animation. I've also started working with the",
+    "Unity game engine, familiarizing myself with C#. Additionally, I'm passionate",
+    "about VR and AR technologies, closely following their advancements and",
+    "exploring their potential to bridge the digital and real world.",
+    "Beyond technical skills, my distinctiveness lies in my enthusiasm for new",
+    "technologies, like exploring the potential of chatbots, especially those",
+    "powered by OpenAI's API (ChatGPT), which has intrigued me for its vast potential",
+    "in interaction and automation.",
+    "<br>"
+  ];
+  
+
+whoami = [
+  "<br>",
+  "Only you can answer that question, but i suspect you are curious.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'portfolio        <a href="' + portfolio + '" target="_blank">portfolio/cleanVersion' + "</a>",
+  'resume        <a href="' + resume + '" target="_blank">dynamic/resume' + '</a>',
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/LyndonSimpson' + "</a>",
+  'game      <a href="' + game + '" target="_blank">games/RPG-Alpha-build' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github/LyndonSimpson' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  'Visit my github to see my publc projects : <a href="' + github + '" target="_blank">github/LyndonSimpson' + "</a>",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Lyndon?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">game</span>           Alpha build of a text RPG I build for my friends',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Do not email me',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">Lyndon Simpson - Not A Corporation. All rights reserved.</span>',
+  ".____                       .___             ",
+  "|    |    ___.__. ____    __| _/____   ____  ",
+  "|    |   <   |  |/    \\  / __ |/  _ \\ /    \\ ",
+  "|    |___ \\___  |   |  \\/ /_/ (  <_> )   |  \\",
+  "|_______ \\/ ____|___|  /\\____ |\\____/|___|  /",
+  "        \\/\\/         \\/      \\/           \\/ @2024",
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
